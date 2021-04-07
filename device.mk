3db07af83bb7e16340836f7425bf194ff7f1854e
LuK1337 <priv.luk@gmail.com>
2021-04-07 11:41:02 +0200

    gts4lv: Vendor tree is not optional
    Change-Id: I14116595bedf7545036c62d62d1e14f6d4dbd06c

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 #
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/samsung/gts4lv/gts4lv-vendor.mk)
+$(call inherit-product, vendor/samsung/gts4lv/gts4lv-vendor.mk)
 
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
