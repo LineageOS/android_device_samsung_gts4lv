3c561edc4c1a2474851631857d2372ac4e44d72d
Bruno Martins <bgcngm@gmail.com>
2021-04-09 16:39:41 +0100

    gts4lv: Update blobs from T725XXU2DUD1
    Change-Id: I509a5d1ea34257e86bc6cbd0d409705f3214141d

--- lineage_gts4lv.mk
+++ lineage_gts4lv.mk
@@ -36,6 +36,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=gts4lvxx \
-    PRIVATE_BUILD_DESC="gts4lvxx-user 10 QP1A.190711.020 T725XXS2CUA2 release-keys"
+    PRIVATE_BUILD_DESC="gts4lvxx-user 11 RP1A.200720.012 T725XXU2DUD1 release-keys"
 
-BUILD_FINGERPRINT := "samsung/gts4lvxx/gts4lv:10/QP1A.190711.020/T725XXS2CUA2:user/release-keys"
+BUILD_FINGERPRINT := "samsung/gts4lvxx/gts4lv:11/RP1A.200720.012/T725XXU2DUD1:user/release-keys"
