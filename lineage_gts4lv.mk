0f5d2abe5b931ecb37ba7b69bffe4e9e5f3ae553
Bruno Martins <bgcngm@gmail.com>
2023-03-18 19:53:15 +0000

    gts4lv: Update blobs from T725XXS3DWA1
    Change-Id: Iba60eb680f62570f33c6d2b20379ee7c2dd42e97

--- lineage_gts4lv.mk
+++ lineage_gts4lv.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020-2022 The LineageOS Project
+# Copyright (C) 2020-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,6 +38,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=gts4lvxx \
-    PRIVATE_BUILD_DESC="gts4lvxx-user 11 RP1A.200720.012 T725XXU2DVG3 release-keys"
+    PRIVATE_BUILD_DESC="gts4lvxx-user 11 RP1A.200720.012 T725XXS3DWA1 release-keys"
 
-BUILD_FINGERPRINT := "samsung/gts4lvxx/gts4lv:11/RP1A.200720.012/T725XXU2DVG3:user/release-keys"
+BUILD_FINGERPRINT := "samsung/gts4lvxx/gts4lv:11/RP1A.200720.012/T725XXS3DWA1:user/release-keys"
